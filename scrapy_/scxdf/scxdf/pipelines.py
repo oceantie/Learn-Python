8eb315f4d240e3e186ada1d263dc17b512361612
Ocean Tie <a2384178@163.com>
2019-04-24 17:53:52 +0800

    download excel

--- scrapy_/scxdf/scxdf/pipelines.py
+++ scrapy_/scxdf/scxdf/pipelines.py
@@ -5,17 +5,27 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exporters import JsonLinesItemExporter
+from openpyxl import Workbook
 
 class ScxdfPipeline(object):
     def __init__(self):
-        self.fp=open('scxdf1.json','wb')
-        self.exporter=JsonLinesItemExporter(self.fp,ensure_ascii=False,encoding='utf-8')
+        # self.fp=open('scxdf1.json','wb')
+        # self.exporter=JsonLinesItemExporter(self.fp,ensure_ascii=False,encoding='utf-8')
+        #
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.ws.append(['article_detail', 'title'])
+
+
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
-        print("="*20)
-        print("write item")
+        line = [item['article_detail'], item['title']]  # 把数据中每一项整理出来
+        self.ws.append(line)  # 将数据以行的形式添加到xlsx中
+        self.wb.save('file.xlsx')  # 保存xlsx文件
+        # print(item)
+        # self.exporter.export_item(item)
         return item
     def close_spider(self,spider):
+        print('cloase')
         self.fp.close()
 
 
